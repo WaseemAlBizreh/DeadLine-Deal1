f9d82fd03bde60e07436de0a8693dd4c94d8f3af
WaseemAlBizreh <waseem.bez@gmail.com>
2021-12-08 22:51:06 +0200

    small edit3

--- lib/MyPages/home.dart
+++ lib/MyPages/home.dart
@@ -9,11 +9,7 @@ class Home extends StatefulWidget{
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Image.asset('assets\imgs\Snap_24.png'),
-      ),
-    );
+    return Scaffold();
   }
 }
 
