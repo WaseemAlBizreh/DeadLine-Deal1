96a68c4419e6f1230e6b340360dbb0dad61ace8f
WaseemAlBizreh <waseem.bez@gmail.com>
2021-12-10 14:07:33 +0200

    log in

--- lib/MyPages/login_page.dart
+++ lib/MyPages/login_page.dart
@@ -1,64 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:splash_screen_view/SplashScreenView.dart';
+import 'package:waseem/main.dart';
 
-import 'MyPages/login_page.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-var c1 = const Color(0xFF06487f);
-var c2 = const Color(0xFF86a9db);
-var c3 = const Color(0xffdcdcf6);
-var c4 = const Color(0xff345b8e);
-var c5 = const Color(0xff5c97d5);
-
-class MyApp extends StatefulWidget {
+class login_page extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
+  State<login_page> createState() => _login_pageState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Product App',
-      debugShowCheckedModeBanner: false,
-      home: SplashScreenView(
-        navigateRoute: Register(),
-        duration: 2000,
-        imageSize: 130,
-        imageSrc: "assets/imgs/1.png",
-        backgroundColor: c3,
-      ),
-    );
-  }
-}
-
-class Register extends StatefulWidget {
-  @override
-  State<Register> createState() => _RegisterState();
-}
-
-class _RegisterState extends State<Register> {
-  bool visible = true;
+class _login_pageState extends State<login_page> {
   bool visible1 = true;
+  var c1 = const Color(0xFF06487f);
+  var c2 = const Color(0xFF86a9db);
+  var c3 = const Color(0xffdcdcf6);
+  var c4 = const Color(0xff345b8e);
+  var c5 = const Color(0xff5c97d5);
 
-  TextEditingController _NameController = TextEditingController();
   TextEditingController _EmailController = TextEditingController();
   TextEditingController _PassController = TextEditingController();
-  TextEditingController _CPassController = TextEditingController();
 
-  final _formkey = GlobalKey<FormState>();
+  final _formkey1 = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Sign up',
+          'Sign in',
           style: TextStyle(fontSize: 25),
         ),
         leading: IconButton(
@@ -79,46 +44,17 @@ class _RegisterState extends State<Register> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 100),
+              SizedBox(height: 200),
               Card(
                 elevation: 75,
                 color: Colors.white.withOpacity(0.4),
                 child: Form(
-                  key: _formkey,
+                  key: _formkey1,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Container(
                         margin: EdgeInsets.fromLTRB(25, 40, 25, 25),
-                        child: TextFormField(
-                          controller: _NameController,
-                          onFieldSubmitted: (value) {
-                            setState(() {
-                              _NameController.text = value;
-                            });
-                          },
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'please enter UserName';
-                            }
-                          },
-                          autocorrect: false,
-                          decoration: InputDecoration(
-                            fillColor: c3,
-                            filled: true,
-                            hintText: "UserName",
-                            prefixIcon: Icon(Icons.person_sharp, color: c4),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(width: 0.4),
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          ),
-                          keyboardType: TextInputType.name,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(25, 0, 25, 25),
                         child: TextFormField(
                           controller: _EmailController,
                           onFieldSubmitted: (value) {
@@ -194,52 +130,13 @@ class _RegisterState extends State<Register> {
                           ),
                         ),
                       ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(25, 0, 25, 25),
-                        child: TextFormField(
-                          controller: _CPassController,
-                          onFieldSubmitted: (value) {
-                            setState(() {
-                              _CPassController.text = value;
-                            });
-                          },
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'please Confirm Password';
-                            }
-                            if (value != _PassController.text) {
-                              return 'Not Match with password';
-                            }
-                          },
-                          autocorrect: false,
-                          obscureText: visible,
-                          decoration: InputDecoration(
-                            fillColor: c3,
-                            filled: true,
-                            hintText: "Confirm Password",
-                            prefixIcon: Icon(Icons.lock, color: c4),
-                            suffixIcon: IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  visible = !visible;
-                                });
-                              },
-                              icon: Icon(
-                                  visible
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                  color: c2),
-                            ),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(width: 0.4),
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          ),
-                          keyboardType: TextInputType.text,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
                       ElevatedButton(
+                        child: const Text(
+                          'Log in',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
                         style: ButtonStyle(
                           shape: MaterialStateProperty.all(
                             RoundedRectangleBorder(
@@ -248,16 +145,10 @@ class _RegisterState extends State<Register> {
                           ),
                           backgroundColor: MaterialStateProperty.all(c1),
                           padding: MaterialStateProperty.all(
-                              const EdgeInsets.fromLTRB(60, 15, 60, 15)),
-                        ),
-                        child: const Text(
-                          'CREATE ACCOUNT',
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
+                              const EdgeInsets.fromLTRB(100, 15, 100, 15)),
                         ),
                         onPressed: () {
-                          _formkey.currentState!.validate();
+                          _formkey1.currentState!.validate();
                         },
                       ),
                       SizedBox(height: 10),
@@ -265,16 +156,16 @@ class _RegisterState extends State<Register> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            "Alredy have have an account?",
+                            "Don't have an account?",
                             style: TextStyle(
                               fontWeight: FontWeight.w500,
                               fontSize: 14,
-                              color: c3,
+                              color: c4,
                             ),
                           ),
                           TextButton(
                               onPressed: () {
-                                Navigator.push(context, MaterialPageRoute(builder: (_)=> login_page()));
+                                Navigator.push(context, MaterialPageRoute(builder: (_)=> Register()));
                               },
                               style: TextButton.styleFrom(
                                 primary: c1,
@@ -283,7 +174,7 @@ class _RegisterState extends State<Register> {
                                 ),
                               ),
                               child: Text(
-                                "Sign in",
+                                "Sign up",
                                 style: TextStyle(
                                     fontWeight: FontWeight.bold,
                                     fontSize: 17,
@@ -291,7 +182,6 @@ class _RegisterState extends State<Register> {
                               )),
                         ],
                       ),
-                      SizedBox(height: 15),
                     ],
                   ),
                 ),
