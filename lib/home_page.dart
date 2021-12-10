a63c22674fc1ed8e5b90afbe86d8edc0b60348e5
BuggyCoder01 <68791440+BuggyCoder01@users.noreply.github.com>
2021-12-10 17:37:12 +0200

    del-of-ElvBtn

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
 import 'u_register.dart';
+
 var c1 = const Color(0xFF06487f);
 
-void selectScreen(BuildContext cntxt){
-  Navigator.of(cntxt).pushReplacement(MaterialPageRoute(
-      builder:(_){
-        return Register();
-      }
-    ));
+void selectScreen(BuildContext cntxt) {
+  Navigator.of(cntxt).pushReplacement(MaterialPageRoute(builder: (_) {
+    return Register();
+  }));
 }
 
 class home_page extends StatefulWidget {
@@ -21,44 +20,45 @@ class _home_pageState extends State<home_page> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          height: double.infinity,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: c1,
-                padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32.0),
-                ),
+        //height: double.infinity,
+        //child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: c1,
+              padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(32.0),
               ),
-              child: const Text(
-                'Register',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              onPressed: () {},
             ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: c1,
-                padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32.0),
-                ),
+            child: const Text(
+              'Register',
+              style: TextStyle(
+                fontSize: 20,
               ),
-              child: const Text(
-                'Register',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              onPressed: () {},
             ),
-          ])
+            onPressed: () {},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: c1,
+              padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(32.0),
+              ),
+            ),
+            child: const Text(
+              'Register',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ])
       ),
+      //),
     );
   }
 }
