52dd079a041c24236cb3fe80ab2c986cb47b8477
BuggyCoder01 <68791440+BuggyCoder01@users.noreply.github.com>
2021-12-18 16:08:10 +0200

    login-request

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -6,7 +6,7 @@ import 'package:waseem/login_model.dart';
 
 class APIService{
   Future<LoginResponseModel> login(LoginRequestModel requestModel)async{
-    String url="https://reqres.in/api/login";
+    String url="https://reqres.in/api/login" ;
     final response = await http.post(Uri.parse(url),body: requestModel.toJson());
   if(response.statusCode==200||response.statusCode==400) {
     return LoginResponseModel.fromJson(convert.jsonDecode(response.body));
