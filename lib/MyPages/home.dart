13b83b8372c4b34850945d3d4a41c28b452839b8
WaseemAlBizreh <waseem.bez@gmail.com>
2021-12-27 00:44:38 +0200

    lastfixed

--- lib/MyPages/home.dart
+++ lib/MyPages/home.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:waseem/Provider/MainProvider.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
