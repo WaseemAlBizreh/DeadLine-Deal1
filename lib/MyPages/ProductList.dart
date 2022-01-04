d6cc28f24472e6417290ed0c3e2f19a4ba9ca267
WaseemAlBizreh <waseem.bez@gmail.com>
2022-01-04 09:22:02 +0200

    productlist v1.1

--- lib/MyPages/ProductList.dart
+++ lib/MyPages/ProductList.dart
@@ -1,36 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:waseem/Model/ProductModel.dart';
 import 'package:waseem/Service/ProductApi.dart';
 import 'package:provider/provider.dart';
-import 'package:waseem/Variables.dart';
 
 import 'AddProductPage.dart';
+import '../Variables.dart';
 
-class Product_list extends StatelessWidget {
+class Product_list extends StatefulWidget {
+  @override
+  State<Product_list> createState() => _Product_listState();
+}
+
+class _Product_listState extends State<Product_list> {
+  late Future fetchData;
+  @override
+  void initState() {
+    super.initState();
+    fetchData = context.read<ProductApiProvider>().ShowAllData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => ProductApiProvider(),
       child: Consumer<ProductApiProvider>(
         builder: (context, data, child) {
-          data.ShowAllData();
           return LayoutBuilder(
             builder: (context, constraints) {
               return Scaffold(
                 appBar: AppBar(
                   backgroundColor: c1,
                   title: Text("Products"),
-                  actions:[Icon(Icons.search)],
+                  actions: [
+                    IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: () {},
+                    ),
+                  ],
                 ),
                 drawer: Drawer(
-                  child: ListView(
-                    children: <Widget>[
-                      DrawerHeader(
-                        decoration: BoxDecoration(
-                          color: c1,
-                        ),
-                        child: Container(
-                          child: Column(
+                    child: ListView(
+                      children: <Widget>[
+                        DrawerHeader(
+                          decoration: BoxDecoration(
+                            color: c1,
+                          ),
+                          child: Container(
+                            child: Column(
                               children: [
                                 CircleAvatar(
                                   radius: 30,
@@ -45,31 +60,112 @@ class Product_list extends StatelessWidget {
                                 )
                               ],
                             ),
+                          ),
                         ),
-                      ),
-                      ListTile(
-                        title: Text(""),
-                      )
-                    ],
-                  )),
-                body: ListView.builder(
-                  itemCount: data.product.length,
-                  itemBuilder: (context,index) {
-                    return Center(
-                      child: Text("hello"),
-                    );
-                  },
-                ),
+                        ListTile(
+                          title: Text(""),
+                        )
+                      ],
+                    )),
+                body: FutureBuilder(
+                    future: fetchData,
+                    builder: (context, snapshot) {
+                      print(data.product.length);
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          itemCount: data.product.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: constraints.maxHeight * 0.15,
+                              width: constraints.maxWidth * 0.5,
+                              margin: EdgeInsets.fromLTRB(
+                                constraints.maxWidth * 0.03,
+                                constraints.maxHeight * 0.01,
+                                constraints.maxWidth * 0.03,
+                                constraints.maxHeight * 0.01,
+                              ),
+                              decoration: BoxDecoration(
+                                color: c3,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: constraints.maxWidth * 0.5,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      child: Image.network(
+                                        data.product[index].image,
+                                        fit: BoxFit.fitWidth,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: constraints.maxWidth * 0.02,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        height: constraints.maxHeight * 0.01,
+                                      ),
+                                      Text(
+                                        data.product[index].name,
+                                        style: TextStyle(
+                                          fontSize: constraints.maxWidth * 0.05,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: constraints.maxHeight * 0.005,
+                                      ),
+                                      Text(
+                                        data.product[index].category,
+                                        style: TextStyle(
+                                          fontSize: constraints.maxWidth * 0.04,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
+                                      Text(
+                                          '\$${data.product[index].init_price}',
+                                          style: TextStyle(
+                                            decoration:
+                                            TextDecoration.lineThrough,
+                                            fontWeight: FontWeight.w400,
+                                          )),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return Center(
+                            child: Text(
+                              '${snapshot.error}',
+                              style: TextStyle(
+                                  fontSize: constraints.maxWidth * 0.05,
+                                  fontWeight: FontWeight.bold),
+                            )
+                        );
+                      }
+                      return Center(
+                          child: CircularProgressIndicator(
+                            backgroundColor: Colors.grey,
+                            color: c1,
+                          )
+                      );
+                    }),
                 floatingActionButton: FloatingActionButton(
                     backgroundColor: c1,
                     tooltip: 'AddProduct',
                     child: const Icon(Icons.add),
-                    onPressed:() {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (_) =>AddProductPage()));
-
-                    }
-                ),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => AddProductPage()));
+                    }),
               );
             },
           );
