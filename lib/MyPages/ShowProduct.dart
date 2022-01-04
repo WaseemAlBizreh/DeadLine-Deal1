e8bffd496234e8f25f03b14440ee0061c4a190e6
WaseemAlBizreh <waseem.bez@gmail.com>
2022-01-04 11:45:15 +0200

    productlist v1.8

--- lib/MyPages/ShowProduct.dart
+++ lib/MyPages/ShowProduct.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:waseem/Model/ProductModel.dart';
 import 'package:waseem/Service/ProductApi.dart';
 
 import '../Variables.dart';
 
-class ShowProduct extends StatelessWidget{
+class ShowProduct extends StatelessWidget {
+  late final ResProduct product;
+
+  ShowProduct({
+    required this.product,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<ProductApiProvider>(
-        builder: (context,data,child){
-          return LayoutBuilder(
-              builder: (context,constraints){
-                return Scaffold(
-                  body: Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: c3,
-                    ),
-                  ),
-                );
-              }
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          return Scaffold(
+            body: Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: c3,
+              ),
+            ),
           );
-        }
-    );
+        });
   }
 }
