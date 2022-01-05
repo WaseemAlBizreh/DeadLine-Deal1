05e62a23d97a0ab38113310b5ff55f38a248b2bf
WaseemAlBizreh <waseem.bez@gmail.com>
2022-01-05 02:18:04 +0200

    showproduct v1.1

--- lib/MyPages/ShowProduct.dart
+++ lib/MyPages/ShowProduct.dart
@@ -7,7 +7,6 @@ import '../Variables.dart';
 
 class ShowProduct extends StatelessWidget {
   late final ResProduct product;
-
   ShowProduct({
     required this.product,
   });
@@ -20,20 +19,16 @@ class ShowProduct extends StatelessWidget {
           backgroundColor: c3,
           elevation: 0,
           leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back,
-              color: c1,
-            ),
+            onPressed: () {Navigator.pop(context);},
+            icon: Icon(Icons.arrow_back, color: c1,),
           ),
           actions: [
             IconButton(
-              icon: Icon(
-                Icons.search,
-                color: c1,
-              ),
+              icon: Icon(Icons.create_sharp , color: c1,),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.delete, color: c1,),
               onPressed: () {},
             ),
           ],
@@ -41,9 +36,7 @@ class ShowProduct extends StatelessWidget {
         body: Container(
           height: double.infinity,
           width: double.infinity,
-          decoration: BoxDecoration(
-            color: c3,
-          ),
+          decoration: BoxDecoration(color: c3,),
           child: SingleChildScrollView(
             child: Column(
               children: [
@@ -61,15 +54,220 @@ class ShowProduct extends StatelessWidget {
                           ),
                         ),
                       ),
-                      alignment:Alignment.topCenter,
+                      alignment: Alignment.topCenter,
                     ),
                     Container(
-                      margin:
-                      EdgeInsets.only(top: constraints.maxHeight * 0.3),
+                      width: double.infinity,
+                      margin: EdgeInsets.only(
+                          top: constraints.maxHeight * 0.3),
                       height: constraints.maxHeight * 0.6,
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(25),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24),
+                        ),
+                      ),
+                      child: Container(
+                        padding:
+                        EdgeInsets.only(left: constraints.maxWidth * 0.1),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: constraints.maxHeight * 0.04,),
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Name\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '${product.name}',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: constraints.maxWidth * 0.1,),
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Category\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '${product.category}',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: constraints.maxHeight * 0.04,),
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'price\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '\$${product.main_price}',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Quantity\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '${product.quantity} ',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Days left\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '${product.days}',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: constraints.maxHeight * 0.04,),
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Expiration date\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '${product.endDate}',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: constraints.maxWidth * 0.1,),
+                                Expanded(
+                                  flex: 1,
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: 'Contact Info\n',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: '${product.contact}',
+                                          style: TextStyle(
+                                            color: c1,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:waseem/Provider/MainProvider.dart';
-// import 'package:intl/intl.dart';
 //import 'package:image_picker/image_picker.dart'
 
 import 'MyPages/ProductList.dart';
