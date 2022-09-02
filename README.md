6063d1e2c5d245de75590b7232d4e33a4f277ad1
Waseem AlBizreh <waseem.bez@gmail.com>
2022-09-02 19:36:06 +0300

    Readme v1.0

--- README.md
+++ README.md
@@ -1,17 +1,189 @@
-# waseem
+<div align="center">
+    <img src=assets/icon/Logo.png alt=“logo” width=“100” height=“auto” />
+    <h1>E-Commerce App<br>DeadLine Deals</h1>
+    <p>
+        Some useful link you have to try it ..
+    </p>
+    <p>
+        <a href="https://github.com/WaseemAlBizreh/DeadLine-Deals/graphs/contributors">
+            <img src="https://img.shields.io/github/contributors/WaseemAlBizreh/DeadLine-Deals" alt="contributors" />
+        </a>
+        <span>..</span>
+        <a href="https://gitlab.com/SuffahSkull/waseem-files/-/tree/main/Aurora%20Warehouse%20files/Demo">View Demo</a>
+    </p>
+</div>
+<br />
 
-A new Flutter project.
+<!-- Table of Contents -->
+# :notebook_with_decorative_cover: Table of Contents
 
-## Getting Started
+- [:notebook_with_decorative_cover: Table of Contents](#notebook_with_decorative_cover-table-of-contents)
+- [:star2: About the Project](#star2-about-the-project)
+- [:camera: Screenshots](#camera-screenshots)
+- [:space_invader: Tech Stack](#space_invader-tech-stack)
+- [:dart: Features](#dart-features)
+- [:eyes: Usage](#eyes-usage)
+- [:wave: projectTeam](#wave-projectteam)
+- [🧭 Roadmap](#-roadmap)
+- [:handshake: Contact](#handshake-contact)
+- [:gem: Acknowledgements](#gem-acknowledgements)
 
-This project is a starting point for a Flutter application.
+<!-- About the Project -->
+## :star2: About the Project
+<p>This Project is for Selling Products That Will Expire Soon where user can create account and
+add product that he want to sell</p>
+<hr><br>
 
-A few resources to get you started if this is your first Flutter project:
+<!-- Screenshots -->
+### :camera: Screenshots
+run vpn If photos don't appear 
+<div>
+<img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/Screenshots/Login.jpg"
+        alt="Login" height="500" width="auto" display="inline"/>
+<img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/Screenshots/View%20Warehouses.jpg"
+        alt="View Warehouses" height="500" width="auto" display="inline"/>
+<img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/Screenshots/Dashboard.jpg"
+        alt="DashBoard" height="500" width="auto" display="inline"/>
+<img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/Screenshots/View%20Products1.jpg"
+        alt="View Products" height="500" width="auto" display="inline"/>
+<img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/Screenshots/Add%20Client.jpg"
+        alt="Add Client" height="500" width="auto" display="inline"/>
+<img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/Screenshots/Add%20Warehouse.jpg"
+        alt="Add warehouse" height="500" width="auto" display="inline"/>
+    <p>more screenshots => This link contains all application interfaces.. </p>
+    <a href="https://gitlab.com/SuffahSkull/waseem-files/-/tree/main/Aurora%20Warehouse%20files/Screenshots">Screenshots</a>
+</div>
+<hr><br>
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+<!-- TechStack -->
+### :space_invader: Tech Stack
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-no use
+<details>
+  <summary>Front-End</summary>
+  <ul>
+    <li><a href="https://dart.dev/">Dart</a></li>
+    <li><a href="https://flutter.dev/">Flutter</a></li>
+  </ul>
+</details>
+<details>
+<summary>Back-End</summary>
+  <ul>
+    <li><a href="https://laravel.com/">Laravel Php</a></li>
+  </ul>
+</details>
+<details>
+<summary>Database</summary>
+  <ul>
+    <li><a href="https://www.mysql.com/">MySQL</a></li>
+  </ul>
+</details>
+<hr><br>
+
+<!-- Features -->
+### :dart: Features
+
+- Create Account , Email Verification, Keeper Register , Login
+- View Warehouse Products , View Warehouse Clients , View Warehouses , View Warehouse Details
+- Add Warehouse , Add Client , Add Category , Add Product ,Add Export Order , Add Receive Order
+- Receive Report , Sort Expiration date , Sort Quantity
+- Search client by name , search client by location
+- Search product by name , search product by category
+- in view product ..border card has red color if Quantity in min or if product close to Expired
+- in view client details or warehouse details ..user can click on number or email and will take him directly to phone or email box
+- the application support arabic and english (localization)
+
+<hr><br>
+
+<!-- Usage -->
+## :eyes: Usage
+<div>
+<h5 >Application architecture</h5>
+    <img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/assets/Application%20architecture.png"
+        alt="Application architecture" height="300" width="auto" display="inline"/>
+<h5>Api End Points</h5>
+    <img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/assets/ApiEndPoint.png"
+        alt="ApiEndPoint" height="300" width="auto" display="inline"/>
+<h5>Application Colors</h5>
+    <img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/assets/color.png"
+        alt="Application Colors" height="300" width="auto" display="inline"/>
+<h5>Application Routes</h5>
+    <img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/assets/routes1.png"
+        alt="Application Routes img1" height="300" width="auto" display="inline"/>
+    <img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/assets/routes2.png"
+        alt="Application Routes img2" height="300" width="auto" display="inline"/>
+<h5>Validation Util</h5>
+    <img src="https://gitlab.com/SuffahSkull/waseem-files/-/raw/main/Aurora%20Warehouse%20files/assets/validation%20Util.png"
+        alt="Validation Util" height="300" width="auto" display="inline"/>
+</div>
+
+<hr><br>
+
+<!-- Contributing -->
+## :wave: projectTeam
+
+<h4>BackEnd Laravel</h4>
+<a href="https://gitlab.com/hiba7">Hiba Allababidi</a>
+<span>.</span>
+<a href="https://gitlab.com/helez22">Helez Tahlo</a>
+<br>
+<h4>FrontEnd Flutter</h4>
+<a href="https://gitlab.com/SuffahSkull">Waseem AlBizreh</a>
+<span>.</span>
+<a href="https://gitlab.com/amaleabannour">Amalea Bannour</a>
+
+
+<!-- Roadmap -->
+## 🧭 Roadmap
+
+* [x] Analysis application
+* [x] Create UseCases
+* [x] Download Useful packages
+* [x] Build Application architecture
+* [x] Build Application interfaces
+* [x] Build Application logic
+* [x] Connecting the interfaces together
+* [x] Connecting the interfaces with the server
+* [x] Test Application
+
+
+<!-- Contact -->
+## :handshake: Contact
+
+Waseem Al-Bizreh - [@linked_in](https://www.linkedin.com/in/waseem-albizreh) - [@Telegram](https://t.me/WaseemAl_Bizreh)
+
+Project Link: [https://gitlab.com/SuffahSkull/warehouse](https://gitlab.com/SuffahSkull/warehouse)
+<hr><br>
+
+<!-- Acknowledgments -->
+## :gem: Acknowledgements
+
+Useful resources and libraries that i have used in My project.
+
+<h4>Packages</h4>
+
+- [Get](https://pub.dev/packages/get)
+- [Lottie](https://pub.dev/packages/lottie)
+- [ScreenUtil](https://pub.dev/packages/flutter_screenutil)
+- [Pin Code Fields](https://pub.dev/packages/pin_code_fields)
+- [Google Fonts](https://pub.dev/packages/google_fonts)
+- [intl](https://pub.dev/packages/intl)
+- [Flutter Toast](https://pub.dev/packages/fluttertoast)
+- [Image Picker](https://pub.dev/packages/image_picker)
+- [Image Cropper](https://pub.dev/packages/image_cropper)
+- [flutter native splash](https://pub.dev/packages/flutter_native_splash)
+- [Get Storage](https://pub.dev/packages/get_storage)
+- [flutter speed dial](https://pub.dev/packages/flutter_speed_dial)
+- [Dio Package](https://pub.dev/packages/dio)
+- [Selection Menu](https://pub.dev/packages/selection_menu)
+- [Url launcher](https://pub.dev/packages/url_launcher)
+- [font awesome flutter](https://pub.dev/packages/font_awesome_flutter)
+- [flutter staggered grid view](https://pub.dev/packages/flutter_staggered_grid_view)
+- [toggle_switch](https://pub.dev/packages/toggle_switch)
+
+<h4>Some Resources</h4>
+- [Lottie Files](https://lottiefiles.com/animation/flutter)
+- [Png Tree](https://pngtree.com/)
+- [Flutter Icons](https://api.flutter.dev/flutter/material/Icons-class.html)
+- [Mock Api](https://mockapi.io/)
+- [StackOverFlow](https://stackoverflow.com/)
+- [000webhost](https://www.000webhost.com/)
--- lib/MyPages/ShowProduct.dart
+++ lib/MyPages/ShowProduct.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:like_button/like_button.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 import 'package:waseem/Model/ProductModel.dart';
@@ -18,30 +19,44 @@ class ShowProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<ProductApiProvider>(
-      builder: (context,data,child){
+      builder: (context, data, child) {
         return LayoutBuilder(builder: (context, constraints) {
           return Scaffold(
             appBar: AppBar(
               backgroundColor: c3,
               elevation: 0,
               leading: IconButton(
-                onPressed: () {Navigator.pop(context);},
-                icon: Icon(Icons.arrow_back, color: c1,),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: c1,
+                ),
               ),
               actions: [
                 IconButton(
-                  icon: Icon(Icons.create_sharp , color: c1,),
+                  icon: Icon(
+                    Icons.create_sharp,
+                    color: c1,
+                  ),
                   onPressed: () {
                     Navigator.push(
-                        context, MaterialPageRoute(builder: (_) =>
-                        EditProductPage(product: product,)));
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => EditProductPage(
+                              product: product,
+                            )));
                   },
                 ),
                 IconButton(
-                  icon: Icon(Icons.delete, color: c1,),
+                  icon: Icon(
+                    Icons.delete,
+                    color: c1,
+                  ),
                   onPressed: () {
-                    data.DeleteProduct(product.id).then((msg){
-                      if(msg){
+                    data.DeleteProduct(product.id).then((msg) {
+                      if (msg) {
                         Fluttertoast.showToast(
                             msg: 'Deleted Successfully',
                             toastLength: Toast.LENGTH_LONG,
@@ -49,11 +64,10 @@ class ShowProduct extends StatelessWidget {
                             timeInSecForIosWeb: 1,
                             backgroundColor: Colors.blueGrey,
                             textColor: Colors.white,
-                            fontSize: 16.0
-                        );
-                        Navigator.push(
-                            context, MaterialPageRoute(builder: (_) => Product_list()));
-                      }else{
+                            fontSize: 16.0);
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => Product_list()));
+                      } else {
                         Fluttertoast.showToast(
                             msg: 'failed Delete try again',
                             toastLength: Toast.LENGTH_LONG,
@@ -61,8 +75,7 @@ class ShowProduct extends StatelessWidget {
                             timeInSecForIosWeb: 1,
                             backgroundColor: Colors.blueGrey,
                             textColor: Colors.white,
-                            fontSize: 16.0
-                        );
+                            fontSize: 16.0);
                       }
                     });
                   },
@@ -70,9 +83,11 @@ class ShowProduct extends StatelessWidget {
               ],
             ),
             body: Container(
-              height: double.infinity,
-              width: double.infinity,
-              decoration: BoxDecoration(color: c3,),
+              height: constraints.maxHeight,
+              width: constraints.maxHeight,
+              decoration: BoxDecoration(
+                color: c3,
+              ),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
@@ -93,9 +108,37 @@ class ShowProduct extends StatelessWidget {
                           alignment: Alignment.topCenter,
                         ),
                         Container(
+                          margin: EdgeInsets.fromLTRB(
+                              constraints.maxWidth * 0.8,
+                              constraints.maxHeight * 0.25,
+                              constraints.minWidth * 0.003,
+                              constraints.minHeight * 0.002),
+                          child: LikeButton(
+                            size: constraints.maxHeight * 0.04,
+                            circleColor: CircleColor(
+                                start: Color(0xff00ddff),
+                                end: Color(0xff0099cc)),
+                            bubblesColor: BubblesColor(
+                              dotPrimaryColor: Color(0xff33b5e5),
+                              dotSecondaryColor: Color(0xff0099cc),
+                            ),
+                            likeBuilder: (isLiked){
+                              data.setid(product.id);
+                              isLiked = product.isLike;
+                              return Icon(
+                                Icons.thumb_up,
+                                color: isLiked ? c1 : Colors.grey,
+                                size: constraints.maxHeight * 0.04,
+                              );
+                            },
+                            likeCount: product.like,
+                            onTap: data.onLikeTap,
+                          ),
+                        ),
+                        Container(
                           width: double.infinity,
-                          margin: EdgeInsets.only(
-                              top: constraints.maxHeight * 0.3),
+                          margin:
+                          EdgeInsets.only(top: constraints.maxHeight * 0.3),
                           height: constraints.maxHeight * 0.6,
                           decoration: BoxDecoration(
                             color: Colors.white,
@@ -105,12 +148,14 @@ class ShowProduct extends StatelessWidget {
                             ),
                           ),
                           child: Container(
-                            padding:
-                            EdgeInsets.only(left: constraints.maxWidth * 0.1),
+                            padding: EdgeInsets.only(
+                                left: constraints.maxWidth * 0.1),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                SizedBox(height: constraints.maxHeight * 0.04,),
+                                SizedBox(
+                                  height: constraints.maxHeight * 0.04,
+                                ),
                                 Row(
                                   children: [
                                     Expanded(
@@ -138,7 +183,9 @@ class ShowProduct extends StatelessWidget {
                                         ),
                                       ),
                                     ),
-                                    SizedBox(width: constraints.maxWidth * 0.1,),
+                                    SizedBox(
+                                      width: constraints.maxWidth * 0.1,
+                                    ),
                                     Expanded(
                                       flex: 1,
                                       child: RichText(
@@ -166,7 +213,9 @@ class ShowProduct extends StatelessWidget {
                                     ),
                                   ],
                                 ),
-                                SizedBox(height: constraints.maxHeight * 0.04,),
+                                SizedBox(
+                                  height: constraints.maxHeight * 0.04,
+                                ),
                                 Row(
                                   children: [
                                     Expanded(
@@ -246,7 +295,9 @@ class ShowProduct extends StatelessWidget {
                                     ),
                                   ],
                                 ),
-                                SizedBox(height: constraints.maxHeight * 0.04,),
+                                SizedBox(
+                                  height: constraints.maxHeight * 0.04,
+                                ),
                                 Row(
                                   children: [
                                     Expanded(
@@ -274,7 +325,9 @@ class ShowProduct extends StatelessWidget {
                                         ),
                                       ),
                                     ),
-                                    SizedBox(width: constraints.maxWidth * 0.1,),
+                                    SizedBox(
+                                      width: constraints.maxWidth * 0.1,
+                                    ),
                                     Container(
                                       // decoration: BoxDecoration(
                                       //   border: Border.all(
@@ -332,15 +385,16 @@ class ShowProduct extends StatelessWidget {
     );
   }
 }
-void Bottonsheets(context){
+
+void Bottonsheets(context) {
   showMaterialModalBottomSheet(
     context: context,
-    builder:(builder){
+    builder: (builder) {
       return new Container(
         height: 450.0,
         color: Colors.transparent, //could change this to Color(0xFF737373),
         //so you don't have to change MaterialApp canvasColor
-        child: new Container(
+        child: Container(
             decoration: new BoxDecoration(
                 color: c3,
                 borderRadius: new BorderRadius.only(
@@ -348,8 +402,7 @@ void Bottonsheets(context){
                     topRight: const Radius.circular(10.0))),
             child: new Center(
               child: new Text("This is a modal sheet"),
-            )
-        ),
+            )),
       );
     },
   );
